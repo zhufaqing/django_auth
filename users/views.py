d9270347fd6c5c3dd8b38696e1b90c2f309fa064
zhufaqing <zhufaqing@163.com>
2019-03-05 11:57:34 +0800

    4.登录功能实现

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegisterForm
 
 # Create your views here.
+def index(request):
+	return render(request,'index.html')
+
+
+def register(request):
+	if request.method == "POST":
+		form = RegisterForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+	else:
+		form = RegisterForm()
+			
+	return render(request,'users/register.html',context={'form':form})
